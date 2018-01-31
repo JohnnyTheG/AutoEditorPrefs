25e0d86f49394837e1ee21361c87cece9f024e46
John Gray <johnny@johnnygray.co.uk>
2018-01-31 21:28:48 +0000

    Added testing files.

---
+++ Assets/Testing/Editor/TestingEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestingEditorWindow : Editor
+{
+	[MenuItem("Tools/AutoEditorPrefs/SetTestData")]
+	public static void SetTestData()
+	{
+		AutoEditorPrefs.SetBool("test", true);
+		AutoEditorPrefs.SetInt("test", 1);
+		AutoEditorPrefs.SetFloat("test", 1.0f);
+		AutoEditorPrefs.SetString("test", "test string");
+	}
+
+	[MenuItem("Tools/AutoEditorPrefs/GetTestData")]
+	public static void GetTestData()
+	{
+		Debug.Log(AutoEditorPrefs.GetBool("test"));
+		Debug.Log(AutoEditorPrefs.GetInt("test"));
+		Debug.Log(AutoEditorPrefs.GetFloat("test"));
+		Debug.Log(AutoEditorPrefs.GetString("test"));
+	}
+
+	[MenuItem("Tools/AutoEditorPrefs/DeleteTestData")]
+	public static void DeleteTestData()
+	{
+		AutoEditorPrefs.DeleteBool("test");
+		AutoEditorPrefs.DeleteInt("test");
+		AutoEditorPrefs.DeleteFloat("test");
+		AutoEditorPrefs.DeleteString("test");
+	}
+}
