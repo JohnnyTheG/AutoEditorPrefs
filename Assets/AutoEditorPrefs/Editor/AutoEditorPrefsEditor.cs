325805538c15ec87ec58251d88814d8d1db4ff07
John Gray <johnny@johnnygray.co.uk>
2018-01-31 20:57:45 +0000

    ScriptableObject and editor script.

---
+++ Assets/AutoEditorPrefs/Editor/AutoEditorPrefsEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+[InitializeOnLoad]
+public static class AutoEditorPrefsEditor
+{
+	public static AutoEditorPrefsScriptableObject m_cAutoEditorPrefsScriptableObject = null;
+
+	static AutoEditorPrefsEditor()
+	{
+		if (m_cAutoEditorPrefsScriptableObject == null)
+		{
+			string[] astrAutoEditorPrefsScriptableObjectPaths = AssetDatabase.FindAssets("t:AutoEditorPrefsScriptableObject");
+
+			if (astrAutoEditorPrefsScriptableObjectPaths.Length > 0)
+			{
+				m_cAutoEditorPrefsScriptableObject = AssetDatabase.LoadAssetAtPath<AutoEditorPrefsScriptableObject>(astrAutoEditorPrefsScriptableObjectPaths[0]);
+			}
+			else
+			{
+				m_cAutoEditorPrefsScriptableObject = ScriptableObject.CreateInstance<AutoEditorPrefsScriptableObject>();
+
+				Directory.CreateDirectory(Application.dataPath + "/AutoEditorPrefs/Settings/");
+
+				AssetDatabase.CreateAsset(m_cAutoEditorPrefsScriptableObject, "Assets/AutoEditorPrefs/Settings/AutoEditorPrefsScriptableObject.asset");
+				AssetDatabase.SaveAssets();
+			}
+		}
+	} 
+}
---
+++ Assets/AutoEditorPrefs/Editor/AutoEditorPrefsScriptableObject.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AutoEditorPrefsScriptableObject : ScriptableObject
+{
+	Dictionary<string, bool> m_dictBools = new Dictionary<string, bool>();
+	Dictionary<string, int> m_dictInts = new Dictionary<string, int>();
+	Dictionary<string, float> m_dictFloats = new Dictionary<string, float>();
+	Dictionary<string, string> m_dictStrings = new Dictionary<string, string>();
+
+
+	public void SetBool(string strKey, bool bValue)
+	{
+		if (!m_dictBools.ContainsKey(strKey))
+		{
+			m_dictBools.Add(strKey, bValue);
+		}
+		else
+		{
+			m_dictBools[strKey] = bValue;
+		}
+	}
+
+	public bool GetBool(string strKey)
+	{
+		if (m_dictBools.ContainsKey(strKey))
+		{
+			return m_dictBools[strKey];
+		}
+
+		return false;
+	}
+
+	public void DeleteBool(string strKey)
+	{
+		if (m_dictBools.ContainsKey(strKey))
+		{
+			m_dictBools.Remove(strKey);
+		}
+	}
+
+	public void SetInt(string strKey, int nValue)
+	{
+		if (!m_dictInts.ContainsKey(strKey))
+		{
+			m_dictInts.Add(strKey, nValue);
+		}
+		else
+		{
+			m_dictInts[strKey] = nValue;
+		}
+	}
+
+	public int GetInt(string strKey)
+	{
+		if (m_dictInts.ContainsKey(strKey))
+		{
+			return m_dictInts[strKey];
+		}
+
+		return 0;
+	}
+
+	public void DeleteInt(string strKey)
+	{
+		if (m_dictInts.ContainsKey(strKey))
+		{
+			m_dictInts.Remove(strKey);
+		}
+	}
+
+	public void SetFloat(string strKey, float fValue)
+	{
+		if (!m_dictFloats.ContainsKey(strKey))
+		{
+			m_dictFloats.Add(strKey, fValue);
+		}
+		else
+		{
+			m_dictFloats[strKey] = fValue;
+		}
+	}
+
+	public float GetFloat(string strKey)
+	{
+		if (m_dictFloats.ContainsKey(strKey))
+		{
+			return m_dictFloats[strKey];
+		}
+
+		return 0.0f;
+	}
+
+	public void DeleteFloat(string strKey)
+	{
+		if (m_dictFloats.ContainsKey(strKey))
+		{
+			m_dictFloats.Remove(strKey);
+		}
+	}
+
+	public void SetString(string strKey, string strValue)
+	{
+		if (!m_dictStrings.ContainsKey(strKey))
+		{
+			m_dictStrings.Add(strKey, strValue);
+		}
+		else
+		{
+			m_dictStrings[strKey] = strValue;
+		}
+	}
+
+	public string GetString(string strKey)
+	{
+		if (m_dictStrings.ContainsKey(strKey))
+		{
+			return m_dictStrings[strKey];
+		}
+
+		return string.Empty;
+	}
+
+	public void DeleteString(string strKey)
+	{
+		if (m_dictStrings.ContainsKey(strKey))
+		{
+			m_dictStrings.Remove(strKey);
+		}
+	}
+}
